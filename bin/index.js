559696fa02a0300a534f7c835fff92025690bd4e
Evan Merz <evanxmerz@yahoo.com>
2022-04-19 09:42:57 -0700

    Added filtering by package name. Added analysis for duplicates.

--- bin/index.js
+++ bin/index.js
@@ -12,8 +12,33 @@ const outputFile = args[1] || "output.html";
 const rawdata = fs.readFileSync(inputFile);
 const bom = JSON.parse(rawdata);
 
+// count the number of components
+const count = bom.components.length;
+
+// find any duplicates
+let duplicateCount = 0;
+let filteredComponents = [];
+let histogram = {};
+for(component of bom.components) {
+  if(!histogram[component.name]) {
+    histogram[component.name] = { ...component, versions: [component.version] };
+  } else {
+    histogram[component.name].versions.push(component.version);
+  }
+}
+
+// push components into filteredComponents array
+for(key in histogram) {
+  filteredComponents.push({ name: key, versions: histogram[key].versions.join(", "), externalReferences: histogram[key].externalReferences });
+
+  // if there are multiple versions of this package, then count it
+  if(Array.isArray(histogram[key].versions) && histogram[key].versions.length > 1) {
+    duplicateCount++;
+  }
+}
+
 // set up the values and template
-const values = { component: bom.metadata.component, components: bom.components, count: bom.components.length };
+const values = { component: bom.metadata.component, components: filteredComponents, count, duplicateCount };
 const template = fs.readFileSync("templates/page.html", 'utf8');
 
 // render the template
