5281fb9d93fe402a91f93e41bc0c8a1581fc26de
Evan Merz <evanxmerz@yahoo.com>
2022-04-18 19:42:52 -0700

    Added comments to index.js

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
-const fs = require('fs');
-const mustache = require('mustache');
+const fs = require('fs'); // needed for reading & writing to files
+const mustache = require('mustache'); // needed for rendering templates
 
 // get the arguments
 const args = process.argv.slice(2);
@@ -12,14 +12,14 @@ const outputFile = args[1] || "output.html";
 const rawdata = fs.readFileSync(inputFile);
 const bom = JSON.parse(rawdata);
 
+// set up the values and template
 const values = { component: bom.metadata.component, components: bom.components };
 const template = fs.readFileSync("templates/page.html", 'utf8');
 
+// render the template
 const output = mustache.render(template, values);
 
-console.log({ output_type: (typeof output) });
-
-
+// write to a file
 fs.writeFileSync(outputFile, output);
 
-process.exit(0); // no errors occurred
+process.exit(0);
