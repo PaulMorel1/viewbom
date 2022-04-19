e1e7aafd64791e7aae352c6ce5db95377877556f
Evan Merz <evanxmerz@yahoo.com>
2022-04-19 10:29:01 -0700

    Added big explanatory comment at the top of index.js. Added exports for each individual method.

--- bin/index.js
+++ bin/index.js
@@ -1,50 +1,92 @@
 #! /usr/bin/env node
-
 const fs = require('fs'); // needed for reading & writing to files
 const mustache = require('mustache'); // needed for rendering templates
 
-// get the arguments
-const args = process.argv.slice(2);
-const inputFile = args[0] || "example/bom.json";
-const outputFile = args[1] || "output.html";
+/*
+  viewBom
+  by Evan X. Merz
+  https://www.npmjs.com/package/viewbom
 
-// read the json in the input file
-const rawdata = fs.readFileSync(inputFile);
-const bom = JSON.parse(rawdata);
+  viewBom is a dead simple package for converting a cyclonedx 
+  software bill of materials into an html file. It also does
+  a small amount of analysis.
 
-// count the number of components
-const count = bom.components.length;
+  It exports three methods:
+  1. analyze = analyzes a clyclonedx sbom as json
+  2. renderToHtml = renders the analysis to an html string
+  3. viewBom = run both of the above methods and save to a file
 
-// find any duplicates
-let duplicateCount = 0;
-let filteredComponents = [];
-let histogram = {};
-for(component of bom.components) {
-  if(!histogram[component.name]) {
-    histogram[component.name] = { ...component, versions: [component.version] };
-  } else {
-    histogram[component.name].versions.push(component.version);
+  For more about cyclonedx bill of materials,
+  see https://www.npmjs.com/package/@cyclonedx/bom
+
+  For more about mustache templating,
+  see https://www.npmjs.com/package/mustache
+*/
+
+function analyze(bom) {
+  // count the number of components
+  const count = bom.components.length;
+
+  // find any duplicates
+  let duplicateCount = 0;
+  let filteredComponents = [];
+  let histogram = {};
+  for(component of bom.components) {
+    if(!histogram[component.name]) {
+      histogram[component.name] = { ...component, versions: [component.version] };
+    } else {
+      histogram[component.name].versions.push(component.version);
+    }
   }
+
+  // push components into filteredComponents array
+  for(key in histogram) {
+    filteredComponents.push({ name: key, versions: histogram[key].versions.join(", "), externalReferences: histogram[key].externalReferences });
+
+    // if there are multiple versions of this package, then count it
+    if(Array.isArray(histogram[key].versions) && histogram[key].versions.length > 1) {
+      duplicateCount++;
+    }
+  }
+
+  // returned the analyzed bom
+  return { component: bom.metadata.component, components: filteredComponents, count, duplicateCount };
 }
 
-// push components into filteredComponents array
-for(key in histogram) {
-  filteredComponents.push({ name: key, versions: histogram[key].versions.join(", "), externalReferences: histogram[key].externalReferences });
-
-  // if there are multiple versions of this package, then count it
-  if(Array.isArray(histogram[key].versions) && histogram[key].versions.length > 1) {
-    duplicateCount++;
-  }
+function renderToHtml(analysis, templatePath) {
+  const template = fs.readFileSync(templatePath, 'utf8');
+  const output = mustache.render(template, analysis);
+  return output;
 }
 
-// set up the values and template
-const values = { component: bom.metadata.component, components: filteredComponents, count, duplicateCount };
-const template = fs.readFileSync("templates/page.html", 'utf8');
+function viewBom(inputFilePath, outputFilePath) {
+  // get the arguments
+  const args = process.argv.slice(2);
+  const inputFile = inputFilePath || args[0] || "example/bom.json";
+  const outputFile = outputFilePath || args[1] || "output.html";
 
-// render the template
-const output = mustache.render(template, values);
+  // read the json in the input file
+  const rawdata = fs.readFileSync(inputFile);
+  const bom = JSON.parse(rawdata);
 
-// write to a file
-fs.writeFileSync(outputFile, output);
+  const analysis = analyze(bom);
+  const html = renderToHtml(analysis, "templates/page.html")
 
-process.exit(0);
+  // write to a file
+  fs.writeFileSync(outputFile, html);
+
+  process.exit(0);
+}
+
+module.exports = [
+  analyze,
+  renderToHtml,
+  viewBom,
+];
+
+// If called using npx, then run viewBom. Otherwise, do nothing
+// See https://stackoverflow.com/questions/6398196/detect-if-called-through-require-or-directly-by-command-line
+if (require.main === module) {
+  viewBom();
+}
+
